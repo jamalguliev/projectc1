7db3d9721bf1de74c61fac6ce29e77294bff0038
jamalguliev <94452049+jamalguliev@users.noreply.github.com>
2021-11-29 14:46:54 +0400

    Add files via upload

--- seasons.cs
+++ seasons.cs
@@ -1,8 +1,32 @@
-﻿'ConsoleApp4.exe' (CoreCLR: DefaultDomain): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Private.CoreLib.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp4.exe' (CoreCLR: clrhost): Loaded 'C:\Users\jamalvg\source\repos\ConsoleApp4\ConsoleApp4\bin\Debug\netcoreapp3.1\ConsoleApp4.dll'. Symbols loaded.
-'ConsoleApp4.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Runtime.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp4.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Console.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp4.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Threading.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp4.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Runtime.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp4.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Text.Encoding.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-The program '[279656] ConsoleApp4.exe' has exited with code 0 (0x0).
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ay = 3;
+            if (ay < 1 || ay > 12)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (ay <= 2 || ay == 12)
+            {
+                Console.WriteLine("Winter");
+            }
+            else if (ay < 6)
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (ay < 9)
+            {
+                Console.WriteLine("Summer");
+            }
+            else
+            {
+                Console.WriteLine("Autumn ");
+            }
+        }
+    }
+}
