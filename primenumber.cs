b8573fd6fe860c40b8d77fc80ce2af6a1fc86c73
jamalguliev <94452049+jamalguliev@users.noreply.github.com>
2021-11-29 14:59:26 +0400

    Add files via upload

--- primenumber.cs
+++ primenumber.cs
@@ -1,8 +1,35 @@
-﻿'ConsoleApp6.exe' (CoreCLR: DefaultDomain): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Private.CoreLib.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp6.exe' (CoreCLR: clrhost): Loaded 'C:\Users\jamalvg\source\repos\ConsoleApp6\ConsoleApp6\bin\Debug\netcoreapp3.1\ConsoleApp6.dll'. Symbols loaded.
-'ConsoleApp6.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Runtime.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp6.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Console.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp6.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Threading.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp6.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Runtime.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'ConsoleApp6.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.19\System.Text.Encoding.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-The program '[272084] ConsoleApp6.exe' has exited with code 0 (0x0).
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        private static int count;
+
+        static void Main(string[] args)
+        {
+            int num = 5;
+            int i = 1; 
+
+
+            for (i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("This is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Prime Number");
+            }
+
+
+        }
+    }
+}
