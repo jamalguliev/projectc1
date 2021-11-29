dab5fdd8410d17b0e13d41a10bdee725d283a9b9
jamalguliev <jamalvg@code.edu.az>
2021-11-29 13:30:15 +0400

    C#

---
+++ Program1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 58364; int sum = 0;
+            for (int i = n; i > 0; i = i / 10)
+            {
+                sum = sum + i % 10;
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
